bf619de3a7b8e0f9fc07663b26c49ac26b39e4f4
ChrisLeeBear <38097622+ChrisLeeBear@users.noreply.github.com>
2018-06-10 23:11:55 +0200

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -4,16 +4,16 @@
 HOST = "irc.twitch.tv"
 PORT = 6667
 NICK = "chris_lee_bot"
-PASS = "oauth:logb99xgzqp6iok60cn4b6bkeuy922"
-CHAN = "sunbrokuu" # sunbrokuu, chris_lee_bear
+PASS = "oauth:xxxx"
+CHAN = "xxx" # xxx, xxx
 RATE = (20/30) # messages per second
-ClientID = "5pwan392ir1zvh2qwpzqer52itk3v5"
-ClientSecrect = "mer48mobpfozxcmxv2j9oh192bq4xi"
+ClientID = "xxx"
+ClientSecrect = "xxx"
 
 # Sql Database Information
 
 DBname = 'postgres'
-DBuser = 'BearBot'
-DBpassword = "JRashWuE2S"
+DBuser = 'xxx'
+DBpassword = "xxx"
 DBhost = 'localhost'
 DBport = '8008'
